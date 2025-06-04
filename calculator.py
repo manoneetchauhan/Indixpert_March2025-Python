5347e37ba55fd2a4587a178786622215ae5c67b4
manoneetchauhan <manoneetchauhan999@gmail.com>
2025-06-04 16:55:54 +0530

    task

---
+++ calculator.py
@@ -0,0 +1,9 @@
+
+firstnumber=int(input("Enter First Number "))
+
+secondnumber=int(input("Enter Second Number "))
+
+result={"sum":firstnumber+secondnumber,"subtract":firstnumber-secondnumber,"multiplication":firstnumber*secondnumber,"division":firstnumber/secondnumber}
+
+print(result)
+
